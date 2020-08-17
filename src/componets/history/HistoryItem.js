b53cfc02cb33bf3a2737b00751733c7b06a5105a
shatanov <timur-shatanov.work@mail.ru>
2020-08-17 17:20:40 +0300

    4 commit

---
+++ src/componets/history/HistoryItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HistoryItem = () => (
+    <li className="history__item history__item-plus">Получил зарплату
+        <span className="history__money">+30000 ₽</span>
+        <button className="history__delete">x</button>
+    </li>
+);
+ export default HistoryItem;
