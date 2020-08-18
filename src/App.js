3732aca2002587688d1d2f12407c4e1dc38f1fb9
shatanov <timur-shatanov.work@mail.ru>
2020-08-18 17:52:19 +0300

    operations

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
-import Total from './componets/total/Total'
-import History from './componets/history/History'
-import Operation from './componets/operation/Operation'
+import Total from './components/total/Total'
+import History from './components/history/History'
+import Operation from './components/operation/Operation'
 
 class App extends Component {
+    state = {
+        transactions: [],
+        description: '',
+        amount: ''
+    }
+    addTransactions = (add) => {
+        const transaction = {
+            id: `key${(+new Date).toString(16)}`,
+            description: this.state.description,
+            amount: this.state.amount,
+            add
+        }
+        const transactions = [...this.state.transactions];
+        transactions.push(transaction);
+
+    }   
+    addAmount = (e) => {
+        this.setState({amount: e.target.value});
+    }
+    addDescription = (e) => {
+        this.setState({description: e.target.value});
+    }
   render(){
     return (
         <>
@@ -16,7 +38,10 @@ class App extends Component {
             <div className="container">
                 <Total/>
                 <History/>
-                <Operation/>
+                <Operation 
+                addTransactions={this.addTransactions}
+                addAmount={this.addAmount}
+                />
             </div>
         </main>
         </>
--- src/componets/history/HistoryItem.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const HistoryItem = () => (
-    <li className="history__item history__item-plus">Получил зарплату
-        <span className="history__money">+30000 ₽</span>
-        <button className="history__delete">x</button>
-    </li>
-);
- export default HistoryItem;
